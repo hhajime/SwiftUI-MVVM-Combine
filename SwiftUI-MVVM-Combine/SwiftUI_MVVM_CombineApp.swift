0c9e5b841f68c23f00dd565b6f7fc80856691f1c
hajime <impapa68@gmail.com>
2023-07-11 01:49:20 +0900

    Initial Commit

---
+++ SwiftUI-MVVM-Combine/SwiftUI_MVVM_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_CombineApp.swift
+//  SwiftUI-MVVM-Combine
+//
+//  Created by Ha Jong Myeong on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
